e0b79186d4ef5506a913f82c0ababba756410803
OddTron <133151370+OddTron@users.noreply.github.com>
2023-05-19 15:17:33 -0700

    working on moo button

--- webpage/FancifyMyText.js
+++ webpage/FancifyMyText.js
@@ -3,14 +3,32 @@ function sendAlert(){
 }
 
 function bigger(){
-    const text = document.getElementById('Text Area')
-    text.style.fontSize = '2em'
+  const text = document.getElementById('Text Area')
+   text.style.fontSize = '2em'
 }
 
 function fancy(){
-
+  const text = document.getElementById('Text Area')
+  text.style.fontWeight = 'bold'
+  text.style.color = 'blue'
+  text.style.textDecoration = 'underline'
+  this.checked = false;
 }
 
 function boring(){
+  const text = document.getElementById('Text Area')
+  text.style.fontWeight = 'normal'
+  text.style.color = 'black'
+  text.style.textDecoration = 'none'
+    this.checked = false;
+}
 
+function moo(){
+  const text = document.getElementById('txtArea')
+  text.style.textTransform = 'uppercase'
+  const sent = t.value.split('.')
+  for (let i = 0; i < sent.length - 1; i++) {
+    sent[i] = sent[i] + '-Moo'
+  }
+  t.value = sent.join('.')
 }
